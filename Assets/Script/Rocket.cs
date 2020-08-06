2c4545af80414aa8a0cd2796356d147bf3cea73b
Apurv singh <apurvsingh9713@gmail.com>
2020-08-06 20:41:19 +0530

    sound effect added and slight change in rocket design

--- Assets/Script/Rocket.cs
+++ Assets/Script/Rocket.cs
@@ -6,34 +6,51 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidbody;
+    AudioSource rocketAudio;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        rocketAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    void Thrust()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up);
+
+            if (!rocketAudio.isPlaying)
+                rocketAudio.Play();
         }
-        
+        else
+        {
+            if (rocketAudio.isPlaying)
+                rocketAudio.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true;
         //Rotate
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
         }
-        else if(Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward);
         }
+        
+        rigidbody.freezeRotation = false;
     }
 }
